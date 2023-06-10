57d81dea3c8f6fddfb5b852c1e83dc7c592b8dff
Yaman Bhardwaj <ymbhardwaj123@gmail.com>
2023-06-10 22:42:38 +0530

    folder created

---
+++ Blog_Applcation/src/main/java/com/Blog/Blog_Applcation/BlogApplcationApplication.java
@@ -0,0 +1,13 @@
+package com.Blog.Blog_Applcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplcationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplcationApplication.class, args);
+	}
+
+}
---
+++ Blog_Applcation/src/test/java/com/Blog/Blog_Applcation/BlogApplcationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blog.Blog_Applcation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplcationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
